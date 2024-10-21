c2d90342b49400738de5a9631cb4332d90bf8f29
Rinx28 <minhanhpreeip@gmail.com>
2024-10-22 01:02:27 +0700

    Update data defautl

--- src/lib/redux/Provider.tsx
+++ src/lib/redux/Provider.tsx
@@ -6,6 +6,17 @@ import { setServer } from './storage/minigame/server';
 import { updateUser } from './storage/user/user';
 import apiClient from '../server/apiClient';
 import { useAppDispatch } from './hook';
+import { setBots } from './storage/eshop/bots';
+import { setClans } from './storage/clan/clans';
+import { setInviteClans } from './storage/clan/invite';
+import { setMsgClans } from './storage/clan/msgClan';
+import { setConfigs } from './storage/eshop/config';
+import { setServices } from './storage/eshop/service';
+import { setMinigames } from './storage/minigame/minigame';
+import { setMessages } from './storage/user/message';
+import { setUserActives } from './storage/user/userActive';
+import { setUserBets } from './storage/user/userBet';
+import { setUserStores } from './storage/user/users';
 
 export default function StoreProvider({
 	children,
@@ -25,6 +36,17 @@ export default function StoreProvider({
 		server = '24'; // Giá trị mặc định nếu không có localStorage
 	}
 	storeRef.current.dispatch(setServer(server));
+	storeRef.current.dispatch(setBots([]));
+	storeRef.current.dispatch(setClans([]));
+	storeRef.current.dispatch(setInviteClans([]));
+	storeRef.current.dispatch(setMsgClans([]));
+	storeRef.current.dispatch(setConfigs([]));
+	storeRef.current.dispatch(setServices([]));
+	storeRef.current.dispatch(setMinigames([]));
+	storeRef.current.dispatch(setMessages([]));
+	storeRef.current.dispatch(setUserActives([]));
+	storeRef.current.dispatch(setUserBets([]));
+	storeRef.current.dispatch(setUserStores([]));
 
 	useEffect(() => {
 		window.scrollTo(0, 0); // Scroll to top when the page reloads
